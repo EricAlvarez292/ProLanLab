965f77357cb816dee7d3d7348b8021d3096597fe
AzureAD\EricAlvarez <eric@migo.io>
2026-04-25 23:12:04 +0800

    Updated other options to be coming soon for now

--- prolanlab-java/src/main/java/com/prolanlab/quizzes/QuizModule.java
+++ prolanlab-java/src/main/java/com/prolanlab/quizzes/QuizModule.java
@@ -1,46 +1,15 @@
 package com.prolanlab.quizzes;
 
-import com.prolanlab.Main;
-
-import java.util.Scanner;
-
 /**
  * Quiz Module - Interactive programming quizzes (placeholder for future development)
  */
 public class QuizModule {
-    private final Scanner scanner = Main.getScanner();
-    
+
     public void start() {
         System.out.println("\n" + "=".repeat(60));
         System.out.println("[Q] QUIZ MODULE");
         System.out.println("=".repeat(60));
         System.out.println();
-        System.out.println(">> Interactive Learning Experience Coming Soon!");
-        System.out.println();
-        System.out.println("The Quiz module will feature:");
-        System.out.println();
-        System.out.println("[M] Multiple Choice Questions");
-        System.out.println("   * Java fundamentals");
-        System.out.println("   * Python basics");
-        System.out.println("   * Programming concepts");
-        System.out.println();
-        System.out.println("[Q] Quick Quiz Mode");
-        System.out.println("   * 10 random questions");
-        System.out.println("   * Instant feedback");
-        System.out.println("   * Score tracking");
-        System.out.println();
-        System.out.println("[C] Challenge Mode");
-        System.out.println("   * Progressive difficulty");
-        System.out.println("   * Coding problems");
-        System.out.println("   * Achievement badges");
-        System.out.println();
-        System.out.println("[P] Progress Tracking");
-        System.out.println("   * Performance analytics");
-        System.out.println("   * Weak areas identification");
-        System.out.println("   * Personalized recommendations");
-        System.out.println();
-        System.out.println("This feature will help reinforce your learning");
-        System.out.println("through interactive practice!");
-        System.out.println();
+        System.out.println(">> Coming Soon!");
     }
 }
